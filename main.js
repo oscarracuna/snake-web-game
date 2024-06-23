a2a73b1619fc358b373b3038110262ad97ede891
oscarracuna <acunainfosec@proton.me>
2024-06-23 17:43:47 -0600

    3rd

---
+++ main.js
@@ -0,0 +1,21 @@
+import './styles.css'
+
+const canvas = document.querySelector('canvas')
+
+const context = canvas.getContext('2d')
+const BOARD_WIDTH = 14
+const BOARD_HEIGHT = 30
+const BLOCK_SIZE = 20
+canvas.width = BLOCK_SIZE * BOARD_WIDTH
+canvas.height = BLOCK_SIZE * BOARD_HEIGHT
+context.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+function update () {
+  draw()
+  window.requestAnimationFrame(update)
+}
+
+function draw () {
+  context.fillStyle = '#000'
+  context.fillRect(0, 0, canvas.width, canvas.height)
+}
